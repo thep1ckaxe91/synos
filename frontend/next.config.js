a226bbd57dd04c94062cce8080d42f9f4bc5679d
thep1ckaxe91 <ThePickaxe91@gmail.com>
2025-11-03 22:21:43 +0700

    feat: cicd for frontend (using App Runner instead of Amplify)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone', // for deployment, dont delete
+
+};
 
 module.exports = nextConfig;
